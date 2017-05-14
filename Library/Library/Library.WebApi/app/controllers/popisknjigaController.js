d7ef2ab5eb4eed8a32c19df6c602ba075aeadeb8
Your Name <you@example.com>
2017-05-14 20:16:28 +0200

    Pagination and search on Books

--- Library/Library/Library.WebApi/app/controllers/popisknjigaController.js
+++ Library/Library/Library.WebApi/app/controllers/popisknjigaController.js
@@ -166,4 +166,11 @@ function popisknjigaController($scope, $http, $stateParams, $window, $state, Aut
         $scope.OdjelEdit = null;
         location.reload(true);
     }
+
+
+    $scope.sort = function (keyname) {
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    }
+
 }
